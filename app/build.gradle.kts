f4b2892b532457498f57f6a6468d9c1519c67842
Eugene Shmelev <shmelevea@gmail.com>
2026-03-17 10:59:21 +0300

    [*] исправлена версия ПО R: обновление в gradle FB: - исправлена версия для публикации

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.example.carsparts"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
